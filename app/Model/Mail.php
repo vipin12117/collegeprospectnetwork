01fab537839288aa77f57ed289b1e57612ea33a7
tathanhxuan <taxuanus@gmail.com>
2014-09-17 01:05:25 +0700

    Mail system

--- app/Model/Mail.php
+++ app/Model/Mail.php
@@ -5,26 +5,5 @@ class Mail extends AppModel{
 	public $name = 'Mail';
 
 	public $useTable = 'mails';
-	
-	public $validate = array(
-		'receiver' => array(
-					'rule'     => 'alphaNumeric',
-					'message'    => 'Please select the Receiver (To)',
-					'required'   => true,
-					'allowEmpty' => false
-		),
-		'subject' => array(
-					'rule'     => 'alphaNumeric',
-					'message'    => 'Please enter the Subject',
-					'required'   => true,
-					'allowEmpty' => false
-		),
-		'message' => array(
-					'rule'     => 'alphaNumeric',
-					'message'    => 'Please enter the Message',
-					'required'   => true,
-					'allowEmpty' => false
-		)
-	);
-	
+			
 }
