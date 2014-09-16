ba34ab5c0bf0539705bcc1ccc89dbbd4ea146b57
Vipin Garg <vipin.garg12@gmail.com>
2014-09-16 22:53:06 +0530

    profile changes

--- app/Controller/ProfileController.php
+++ app/Controller/ProfileController.php
@@ -6,7 +6,7 @@ class ProfileController extends AppController{
 
 	public $user_type = false;
 
-	public $uses = array('Mail','Network','CollegeCoach','Athlete','HsAauCoach','Event','Banner','AthleteVideo','Note','AthleteStat');
+	public $uses = array('Mail','Network','CollegeCoach','Athlete','HsAauCoach','Event','Banner','AthleteVideo','Note','AthleteStat','HsAauCoachSportposition');
 
 	public $components = array('Email','Session','Image');
 
@@ -29,7 +29,7 @@ class ProfileController extends AppController{
 		$this->set("mailCount",$mailCount);
 
 		$user_id = $this->Session->read('user_id');
-		$networkCount = $this->Network->find("count",array("conditions"=>"Network.receiver_id = '$user_id' and Network.status = 'unread'"));
+		$networkCount = $this->Network->find("count",array("conditions"=>"Network.receiver_id = '$user_id' and Network.status = 'Active'"));
 		$this->set("networkCount",$networkCount);
 
 		$is_trial_mode = false;
@@ -41,7 +41,6 @@ class ProfileController extends AppController{
 			if($total_days > (5*24*60*60)){
 				$is_trial_mode = true;
 			}
-			$this->set("is_trial_mode",$is_trial_mode);
 		}
 		elseif($this->user_type == 'CollegeCoach'){
 			$profileDetail = $this->CollegeCoach->getByUsername($username);
@@ -52,6 +51,7 @@ class ProfileController extends AppController{
 			$this->set("profileDetail",$profileDetail);
 		}
 
+		$this->set("is_trial_mode",$is_trial_mode);
 		$this->render("/Profile/index");
 	}
 
@@ -121,32 +121,106 @@ class ProfileController extends AppController{
 		}
 	}
 
-	public function hsAauCoachProfile(){
+	public function hsAauCoachProfile($user_id = false){
+		$user_id = (int)$user_id;
+		if(!$user_id){
+			$user_id  = $this->Session->read('user_id');
+			$username = $this->Session->read("username");
+		}
 
+		$profileDetail = $this->HsAauCoach->getById($user_id);
+		$this->set("profileDetail",$profileDetail);
 
 		$this->render("/Profile/hsAauCoachProfile");
 	}
 
 	public function editHsAauCoachProfile(){
 		$user_id  = $this->Session->read('user_id');
-		$profileDetail = $this->Athlete->getById($user_id);
-		$this->set("profileDetail",$profileDetail);
 
-		$this->render("/Profile/editHsAauCoachProfile");
+		if(isset($this->request->data['HsAauCoach'])){
+			$this->request->data['HsAauCoach']['id'] = $user_id;
+
+			//unset these fields
+			unset($this->request->data['HsAauCoach']['username']);
+			unset($this->request->data['HsAauCoach']['email']);
+				
+			$sports = $this->request->data['HsAauCoach']['sport_id'];
+			$positions = $this->request->data['HsAauCoach']['position'];
+
+			$this->request->data['HsAauCoach']['sport_id'] = $this->request->data['HsAauCoach']['sport_id'][0];
+			$this->request->data['HsAauCoach']['position'] = $this->request->data['HsAauCoach']['position'][0];
+
+			$this->HsAauCoach->save($this->request->data);
+
+			$sportPositions = array();
+			//insert all sports
+			foreach($sports as $i => $value){
+				$sportPosition = array();
+				$sportPosition['sport_id'] = $value;
+				$sportPosition['position'] = $positions[$i];
+				$sportPosition['hs_aau_coach_id'] = $user_id;
+
+				try{
+					$this->HsAauCoachSportposition->create();
+					$this->HsAauCoachSportposition->save(array("HsAauCoachSportposition"=>$sportPosition));
+				}
+				catch(Exception $e){
+
+				}
+
+				$sportPositions[] = $sportPosition;
+			}
+
+			$this->Session->setFlash("Profile is updated successfully");
+			$this->redirect(array("controller"=>"Profile","action"=>"hsAauCoachProfile"));
+			exit;
+		}
+		else{
+			$this->request->data = $this->HsAauCoach->getById($user_id);
+			$this->render("/Profile/editHsAauCoachProfile");
+		}
 	}
 
-	public function collegeCoachProfile(){
+	public function collegeCoachProfile($user_id = false){
+		$user_id = (int)$user_id;
+		if(!$user_id){
+			$user_id  = $this->Session->read('user_id');
+			$username = $this->Session->read("username");
+		}
 
+		$profileDetail = $this->CollegeCoach->getById($user_id);
+		$this->set("profileDetail",$profileDetail);
+		
+		$this->loadModel('CollegeNeed');
+		$collegeNeeds = $this->CollegeNeed->getByCollegeId($profileDetail['CollegeCoach']['college_id']);
+		$this->set("collegeNeeds",$collegeNeeds);
+		
+		$networkRequests = $this->Network->find("all",array("conditions"=>"Network.receiver_id = '$user_id' and Network.status = 'Active'"));
+		$this->set("networkRequests",$networkRequests);
 
 		$this->render("/Profile/collegeCoachProfile");
 	}
 
 	public function editCollegeCoachProfile(){
 		$user_id  = $this->Session->read('user_id');
-		$profileDetail = $this->Athlete->getById($user_id);
-		$this->set("profileDetail",$profileDetail);
 
-		$this->render("/Profile/editCollegeCoachProfile");
+		if(isset($this->request->data['CollegeCoach'])){
+			$this->request->data['CollegeCoach']['id'] = $user_id;
+
+			//unset these fields
+			unset($this->request->data['CollegeCoach']['username']);
+			unset($this->request->data['CollegeCoach']['email']);
+
+			$this->CollegeCoach->save($this->request->data);
+
+			$this->Session->setFlash("Profile is updated successfully");
+			$this->redirect(array("controller"=>"Profile","action"=>"collegeCoachProfile"));
+			exit;
+		}
+		else{
+			$this->request->data = $this->CollegeCoach->getById($user_id);
+			$this->render("/Profile/editCollegeCoachProfile");
+		}
 	}
 
 	public function changePassword(){
