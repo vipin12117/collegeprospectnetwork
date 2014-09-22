ba9459a9b0e6b217dec5b2f21f883f11126ade27
tathanhxuan <taxuanus@gmail.com>
2014-09-22 23:51:47 +0700

    Admin part

--- app/Model/Admin.php
+++ app/Model/Admin.php
@@ -5,9 +5,5 @@ class Admin extends AppModel{
 	public $name = 'Admin';
 
 	public $useTable = 'admin';
-
-	public function __construct(){
-		parent::__construct();
-		$this->primaryKey = 'id';
-	}
+	
 }
