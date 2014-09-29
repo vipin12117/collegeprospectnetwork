fb0bc4a99ac8e44609f0eb7440a00ab83212d108
Vipin Garg <vipin.garg12@gmail.com>
2014-09-29 22:42:15 +0530

    subscription changes

--- app/Model/CollegeSubscription.php
+++ app/Model/CollegeSubscription.php
@@ -15,10 +15,10 @@ class CollegeSubscription extends AppModel{
 
 	public function getDetailByCollegeCoachId($college_coach_id , $sport_id = false){
 		if($sport_id){
-			$row = $this->find("first",array("conditions"=>"CollegeSubscription.college_coach_id = '$college_coach_id' AND CollegeSubscription.sport_id = '$sport_id' AND CollegeSubscription.status = 1"));
+			$row = $this->find("first",array("conditions"=>"CollegeSubscription.college_coach_id = '$college_coach_id' AND CollegeSubscription.sport_id = '$sport_id' AND CollegeSubscription.status = 1","order"=>"next_billdate DESC"));
 		}
 		else{
-			$row = $this->find("first",array("conditions"=>"CollegeSubscription.college_coach_id = '$college_coach_id' AND CollegeSubscription.status = 1"));
+			$row = $this->find("first",array("conditions"=>"CollegeSubscription.college_coach_id = '$college_coach_id' AND CollegeSubscription.status = 1","order"=>"next_billdate DESC"));
 		}
 		
 		return $row;
