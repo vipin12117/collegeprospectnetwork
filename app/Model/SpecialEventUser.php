7ff67a72c660bf71d53f062cb9d575ccb5670ff7
tathanhxuan <taxuanus@gmail.com>
2014-12-22 18:58:16 +0700

    special event

--- app/Model/SpecialEventUser.php
+++ app/Model/SpecialEventUser.php
@@ -5,11 +5,18 @@ class SpecialEventUser extends AppModel{
 	public $name = 'SpecialEventUser';
 
 	public $useTable = 'special_event_users';
-	
-	//public $belongsTo = array('TransportationDiscount');
 
-	public function __construct(){
-		parent::__construct();
-		$this->primaryKey = 'id';
+	public function specialEventReport(){
+		
+		$sql = 	"SELECT *						 
+				FROM 
+					special_event_users sr,
+					special_events se					 
+				WHERE 
+					sr.payment_status = '1' AND  
+					sr.special_event_id !='' AND 
+					se.status = '1' 
+				ORDER BY sr.id";
+		return $this->query($sql);
 	}
 }
